c356cbaae1b8be3d1f2e060b0c6432c30a480d7a
Davi Segantin <davi.g.segantin@gmail.com>
2024-05-07 11:40:55 -0300

    primeiro commit

---
+++ src/main/java/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,11 @@
+package com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("Year") String ano,
+                         @JsonAlias("totalSeasons") Integer totalDeTemporadas,
+                         @JsonAlias("imdbRating") String avaliacaoImdb) {
+}
